8c7e826f68e03363bbab74c5b9b9cf6c3c29ebd5
Olivia FUNG <olivia@hk.riege.com>
2018-07-14 18:34:01 +0800

    Initial Commit

---
+++ pokedex4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex4
+//
+//  Created by Olivia FUNG on 14/7/2018.
+//  Copyright © 2018年 Olivia FUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
